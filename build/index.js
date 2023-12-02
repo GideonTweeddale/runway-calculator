dd07de80c7fc65acab5d9735d8ae0d7e8b731607
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2023-12-02 14:55:11 -0500

    Add income

--- build/index.js
+++ build/index.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Selecting elements
   const cashInput = document.getElementById("cash");
   const burnRateInput = document.getElementById("burnRate");
+  const incomeInput = document.getElementById("income");
   const resultYears = document.getElementById("result-years");
-  //   const resultMonths = document.getElementById("result-months");
   const resultWeeks = document.getElementById("result-weeks");
   const resultDays = document.getElementById("result-days");
   const resultHours = document.getElementById("result-hours");
@@ -12,68 +12,70 @@ document.addEventListener("DOMContentLoaded", function () {
   const resultSeconds = document.getElementById("result-seconds");
   const graphCanvas = document.getElementById("graph");
 
-  // initialise the chart
-  plotChart([])
-
   // Event listener on input change
   cashInput.addEventListener("input", updateResults);
   burnRateInput.addEventListener("input", updateResults);
+  incomeInput.addEventListener("input", updateResults);
 
-  // Initial results and graph update
+  // initial chart
+  plotChart([])
+
+  // initial results
   updateResults();
 
   function updateResults() {
     const cash = parseFloat(cashInput.value) || 0;
-    const burnRate = parseFloat(burnRateInput.value) || 0;
+    const dailySpend = parseFloat(burnRateInput.value) || 0;
+    const weeklyIncome = parseFloat(incomeInput.value) || 0;
 
     if (cash > 0) {
-        // Calculate time to burn
-        const yearsToBurn = cash / ( burnRate * 365 );
-        // const monthsToBurn = cash / burnRate;
-        const weeksToBurn = cash / ( burnRate * 7 );
-        const daysToBurn = cash / burnRate;
-        const hoursToBurn = cash / ( burnRate / 24 );
-        const minutesToBurn = hoursToBurn * 60;
-        const secondsToBurn = minutesToBurn * 60;
-    
+      const dailyBurn = ( dailySpend - ( weeklyIncome / 7 ) );
+
+      // Calculate time to burn
+      const yearsToBurn = cash / ( ( dailySpend * 365 ) - ( weeklyIncome * 52.1429 ) );
+      const weeksToBurn = cash / ( dailyBurn * 7 );
+      const daysToBurn = cash / dailyBurn;
+      const hoursToBurn = daysToBurn * 24;
+      const minutesToBurn = hoursToBurn * 60;
+      const secondsToBurn = minutesToBurn * 60;
+        
+      if (dailyBurn > 0) {    
         // Display time to burn
         resultYears.innerHTML = `Time to Burn: <span class="text-green-600">${yearsToBurn.toFixed(1)}</span> years`;
-        // resultMonths.textContent = `Time to Burn: ${monthsToBurn.toFixed(1)} months`;
         resultWeeks.innerHTML = `Time to Burn: <span class="text-green-600">${weeksToBurn.toFixed(1)}</span> weeks`;
         resultDays.innerHTML = `Time to Burn: <span class="text-green-600">${daysToBurn.toFixed(1)}</span> days`;
         resultHours.innerHTML = `Time to Burn: <span class="text-green-600">${hoursToBurn.toFixed(0)}</span> hours`;
         resultMinutes.innerHTML = `Time to Burn: <span class="text-green-600">${minutesToBurn.toFixed(0)}</span> mintes`;
         resultSeconds.innerHTML = `Time to Burn: <span class="text-green-600">${secondsToBurn.toFixed(0)}</span> seconds`;
+      } else {
+        resultYears.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> years`;
+        resultWeeks.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> weeks`;
+        resultDays.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> days`;
+        resultHours.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> hours`;
+        resultMinutes.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> mintes`;
+        resultSeconds.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> seconds`;
+      }
+
+      destroyChart();
     
-        // Generate graph
-        if (daysToBurn > 0 && cash > 0) {
-            generateGraph(daysToBurn, cash, burnRate);
-        }
+      // Generate graph
+      if (daysToBurn > 0 && cash > 0 && dailyBurn > 0 && yearsToBurn < 20) {
+        console.log("Days to burn: ", daysToBurn);
+        generateGraph(daysToBurn, cash, dailyBurn);
+      }
     }
   }
 
-  function generateGraph(timeToBurn, initialCash, burnRate) {
-    // Destroy existing chart if it exists
-    if (window.myChart) {
-      window.myChart.destroy();
-    }
-
+  function generateGraph(daysToBurn, initialCash, burnRate) {
     // Initialize an array to store the daily cash information
     let dailyCashArray = [];    
-    let days = Array.from({ length: Math.ceil(timeToBurn) }, (_, i) => i + 1);
+    let days = Array.from({ length: Math.ceil(daysToBurn) }, (_, i) => i + 1);
     
     // Loop through each day and calculate the remaining cash
     days.forEach(day => {
-        // if (day === 1) {
-        //     console.log(initialCash, day, burnRate, (day * burnRate), (initialCash - day * burnRate))
-        // }
-
         const remainingCash = Math.max(0, initialCash - day * burnRate); // Ensure cash doesn't go below 0
         dailyCashArray.push({ day, cash: remainingCash });
     });
-    
-    // Display the array of objects
-    console.log(dailyCashArray);
 
     plotChart(dailyCashArray);
   }
@@ -109,5 +111,12 @@ document.addEventListener("DOMContentLoaded", function () {
   
       // Create chart
       window.myChart = new Chart(graphCanvas, config);
+  }
+
+  function destroyChart() {    
+    // Destroy existing chart if it exists
+    if (window.myChart) {
+      window.myChart.destroy();
+    }
   }
 });
