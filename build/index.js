6f678a2da3ef4779c9feacd6853ca95760e6cf97
Gideon Tweeddale <gideon.tweeddale@gmail.com>
2023-12-02 18:20:11 -0500

    Add icon Change to three columns Add monthly income Change chart colour

--- build/index.js
+++ build/index.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const burnRateInput = document.getElementById("burnRate");
   const incomeInput = document.getElementById("income");
   const resultYears = document.getElementById("result-years");
+  const resultMonths = document.getElementById("result-months");
   const resultWeeks = document.getElementById("result-weeks");
   const resultDays = document.getElementById("result-days");
   const resultHours = document.getElementById("result-hours");
   const resultMinutes = document.getElementById("result-minutes");
   const resultSeconds = document.getElementById("result-seconds");
-  const graphCanvas = document.getElementById("graph");
 
   // Event listener on input change
   cashInput.addEventListener("input", updateResults);
@@ -26,109 +26,112 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateResults() {
     const cash = parseFloat(cashInput.value) || 0;
     const dailySpend = parseFloat(burnRateInput.value) || 0;
-    const weeklyIncome = parseFloat(incomeInput.value) || 0;
+    const monthlyIncome = parseFloat(incomeInput.value) || 0;
 
     if (cash > 0) {
-      const dailyBurn = ( dailySpend - ( weeklyIncome / 7 ) );
+      const dailyCashSet = generateDailyCashDataset(cash, dailySpend, monthlyIncome);
 
       // Calculate time to burn
-      const yearsToBurn = cash / ( ( dailySpend * 365 ) - ( weeklyIncome * 52.1429 ) );
-      const weeksToBurn = cash / ( dailyBurn * 7 );
-      const daysToBurn = cash / dailyBurn;
-      const hoursToBurn = daysToBurn * 24;
-      const minutesToBurn = hoursToBurn * 60;
-      const secondsToBurn = minutesToBurn * 60;
-        
-      if (dailyBurn > 0) {    
+      const daysToBurn = dailyCashSet.length;
+
+      if (daysToBurn < 3651) {
+        const yearsToBurn = daysToBurn / 365;
+        const monthToBurn = yearsToBurn * 12;
+        const weeksToBurn = daysToBurn / 7;
+        const hoursToBurn = daysToBurn * 24;
+        const minutesToBurn = hoursToBurn * 60;
+        const secondsToBurn = minutesToBurn * 60;
+           
         // Display time to burn
-        resultYears.innerHTML = `Time to Burn: <span class="text-green-600">${yearsToBurn.toFixed(1)}</span> years`;
-        resultWeeks.innerHTML = `Time to Burn: <span class="text-green-600">${weeksToBurn.toFixed(1)}</span> weeks`;
-        resultDays.innerHTML = `Time to Burn: <span class="text-green-600">${daysToBurn.toFixed(1)}</span> days`;
-        resultHours.innerHTML = `Time to Burn: <span class="text-green-600">${hoursToBurn.toFixed(0)}</span> hours`;
-        resultMinutes.innerHTML = `Time to Burn: <span class="text-green-600">${minutesToBurn.toFixed(0)}</span> mintes`;
-        resultSeconds.innerHTML = `Time to Burn: <span class="text-green-600">${secondsToBurn.toFixed(0)}</span> seconds`;
-      } else {
-        resultYears.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> years`;
-        resultWeeks.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> weeks`;
-        resultDays.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> days`;
-        resultHours.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> hours`;
-        resultMinutes.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> mintes`;
-        resultSeconds.innerHTML = `Time to Burn: <span class="text-green-600">Infinity</span> seconds`;
+        resultYears.innerHTML = `Time to burn: <span class="text-green-600">${yearsToBurn.toFixed(1)}</span> years`;
+        resultMonths.innerHTML = `Time to burn: <span class="text-green-600">${monthToBurn.toFixed(1)}</span> months`;
+        resultWeeks.innerHTML = `Time to burn: <span class="text-green-600">${weeksToBurn.toFixed(1)}</span> weeks`;
+        resultDays.innerHTML = `Time to burn: <span class="text-green-600">${daysToBurn.toFixed(1)}</span> days`;
+        resultHours.innerHTML = `Time to burn: <span class="text-green-600">${hoursToBurn.toFixed(0)}</span> hours`;
+        resultMinutes.innerHTML = `Time to burn: <span class="text-green-600">${minutesToBurn.toFixed(0)}</span> minutes`;
+        resultSeconds.innerHTML = `Time to burn: <span class="text-green-600">${secondsToBurn.toFixed(0)}</span> seconds`;
+      
+        // Generate graph
+        destroyChart();
+        plotChart(dailyCashSet);
       }
-
-      destroyChart();
-    
-      // Generate graph
-      if (daysToBurn > 0 && cash > 0 && dailyBurn > 0 && yearsToBurn < 20) {
-        console.log("Days to burn: ", daysToBurn);
-        generateGraph(daysToBurn, cash, dailyBurn);
-      }
-    }
-  }
-
-  function generateGraph(daysToBurn, initialCash, burnRate) {
-    // Initialize an array to store the daily cash information
-    let dailyCashArray = [];    
-    let days = Array.from({ length: Math.ceil(daysToBurn) }, (_, i) => i + 1);
-    
-    // if (days.length < 365) {
-
-    // } else if (days.length < 1095) {
-
-    // } else if (days.length < 3650) {
-
-    // } else {
-
-    // }
-    
-    // Loop through each day and calculate the remaining cash
-    days.forEach(day => {
-        const remainingCash = Math.max(0, initialCash - day * burnRate); // Ensure cash doesn't go below 0
-        // let currentDay = removeTime();
-        // currentDay.setDate( currentDay.getDate() + ( day - 1 ));
-        let currentDay = moment().add((day-1), 'days').format('D-MM-YYYY');
-
-        if (day < 5) {
-          console.log(currentDay)
-        }
-
-        dailyCashArray.push({ day: currentDay, cash: remainingCash.toFixed(0) });
-    });
-
-    console.log(dailyCashArray)
-
-    plotChart(dailyCashArray);
-  }
-
-  function plotChart(data) {
-    // Chart.js configuration
-    const config = {
-        type: "line",
-        data: {
-          labels: data.map(row => row.day),
-          datasets: [
-            {
-              label: "Runway",
-              data: data.map(row => row.cash),
-              backgroundColor: "#ea580c",
-              borderColor: "rgba(255, 99, 132, 1)",
-              borderWidth: 1,
-            },
-          ],
-        }
-      };
+      else {
+        console.log("You aren't going to run out of money.")
   
-      // Create chart
-      window.myChart = new Chart(graphCanvas, config);
-  }
-
-  function destroyChart() {    
-    // Destroy existing chart if it exists
-    if (window.myChart) {
-      window.myChart.destroy();
-    }
+        // Generate graph
+        destroyChart();
+  
+        resultYears.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> years`;
+        resultMonths.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> months`;
+        resultWeeks.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> weeks`;
+        resultDays.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> days`;
+        resultHours.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> hours`;
+        resultMinutes.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> mintes`;
+        resultSeconds.innerHTML = `Time to burn: <span class="text-green-600">infinite</span> seconds`;
+      }
+    } 
   }
 });
+
+function generateDailyCashDataset(initialCash, dailySpend, monthlyIncome) {
+  // Initialize an array to store the daily cash information
+  let dailyCashArray = [];    
+  let remainingCash = initialCash;
+  let day = 1;
+  
+  // Loop through each day and calculate the remaining cash until you run out of cash or hit ten years
+  while (remainingCash > 0 && day < 3652) {
+    remainingCash = Math.max(0, remainingCash - dailySpend);
+    let currentDay = moment().add((day), 'days').format('D-MM-YYYY');
+    day += 1; // Increment day.
+    
+    // Add income
+    if (currentDay.toString().split('-')[0] === '1') {
+      remainingCash = remainingCash + monthlyIncome;
+    }
+
+    dailyCashArray.push({ day: currentDay, cash: remainingCash.toFixed(0) })
+  }
+
+  return dailyCashArray;
+}
+
+function getMonthlyInterestRate() {
+  let interest = parseFloat(document.getElementById("interest").value) || 0; // Annual interest
+  interest = Math.pow((1 + interest), (1 / 12)) - 1; // Monthly interest.
+
+  interest = interest.toFixed(4);
+  return interest;
+}
+
+function plotChart(data) {
+  // Chart.js configuration
+  const config = {
+      type: "line",
+      data: {
+        labels: data.map(row => row.day),
+        datasets: [
+          {
+            label: "Runway",
+            data: data.map(row => row.cash),
+            backgroundColor: "#16a34a",
+            borderColor: "#16a34a",
+            borderWidth: 1,
+          },
+        ],
+      }
+    };
+
+    // Create chart
+    window.myChart = new Chart(document.getElementById("graph"), config);
+}
+
+function destroyChart() {    
+  // Destroy existing chart if it exists
+  if (window.myChart) {
+    window.myChart.destroy();
+  }
+}
 
 function removeTime(date = new Date()) {
   return new Date(
